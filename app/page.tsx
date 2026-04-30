409d26a7b064be44ea49c21c14a8955cc87a92c6
Santiago Vidaurre <vidaurre.santi@gmail.com>
2026-04-30 13:58:56 -0300

    Nuevo home

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main style={{ textAlign: "center", marginTop: "100px" }}>
-      <h1>Hola, soy Santi VVVii 👋</h1>
+      <h1>Hola, soy Santi VVViiui 👋</h1>
       <p>Mi primera app con Next.js 🚀</p>
     </main>
   );
