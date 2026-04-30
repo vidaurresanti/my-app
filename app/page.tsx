63e7456df99ba1bea4cdfee1ded65a3c1b4bb72e
Santiago Vidaurre <vidaurre.santi@gmail.com>
2026-04-30 14:08:25 -0300

    Agrego navbar

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <main style={{ textAlign: "center", marginTop: "100px" }}>
       <h1>Hola, soy Santi VVViiui 👋</h1>
+      <p>Mi primera app con Next.js fff🚀</p>
       <p>Mi primera app con Next.js 🚀</p>
     </main>
   );
