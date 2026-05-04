a597554846541c5ae25cf2c1e5b8efd69f34700b
Santiago Vidaurre <vidaurre.santi@gmail.com>
2026-05-03 22:27:24 -0300

    Actualización de Principal

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 ﻿import Footer from "@/components/ui/footer";
+import Header from "@/components/ui/header";
 import Link from "next/link";
 
 export default function Home() {
@@ -6,17 +7,9 @@ export default function Home() {
     <div className="flex flex-col min-h-screen bg-gray-100">
 
       {/* HEADER */}
-      <header className="sticky top-0 z-50 bg-white shadow-md">
-        <div className="max-w-6xl mx-auto flex justify-between items-center p-4">
-          <h1 className="text-xl font-bold">Mi App</h1>
-
-          <nav className="flex gap-6">
-            <Link href="/">Home</Link>
-            <Link href="/servicios">Servicios</Link>
-            <Link href="/contacto">Contacto</Link>
-          </nav>
-        </div>
-      </header>
+      <div>
+        <Header />
+      </div>
 
       <main className="flex-1 p-8">
         <section className="text-center max-w-6xl mx-auto">
